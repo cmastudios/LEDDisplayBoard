1871dba9302bde6423855b1387897d4902f4ad91
Connor Monahan <cma2714@gmail.com>
2020-12-26 16:27:42 -0600

    Still fighting tabs n spaces

--- Software/RinaBitBang.X/images.s
+++ Software/RinaBitBang.X/images.s
@@ -2,33 +2,33 @@
 # we have 270kB to play with on this overspecced CPU, each image weighs 660b
 # so we can fit over 400 images if we need
 	.section .rodata
-    
+
 	.global image_face
 	.type   image_face, %object
-    .align  4
+	.align  4
 image_face:
-    .incbin "images/face.bin"
-    
+	.incbin "images/face.bin"
+
 	.global image_cse467
 	.type   image_cse467, %object
-    .align  4
+	.align  4
 image_cse467:
-    .incbin "images/cse467.bin"
+	.incbin "images/cse467.bin"
 
 	.global image_washu
 	.type   image_washu, %object
-    .align  4
+	.align  4
 image_washu:
-    .incbin "images/washu.bin"
+	.incbin "images/washu.bin"
 
 	.global image_error_battery
 	.type   image_error_battery, %object
-    .align  4
+	.align  4
 image_error_battery:
-    .incbin "images/lowbattery.bin"
+	.incbin "images/lowbattery.bin"
 
 	.global image_error_wireless
 	.type   image_error_wireless, %object
-    .align  4
+	.align  4
 image_error_wireless:
-    .incbin "images/wirelesserror.bin"
+	.incbin "images/wirelesserror.bin"
